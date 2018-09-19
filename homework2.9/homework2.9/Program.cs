a0653b39267dc27c5c5431af08c7b2e7984b506f
Your Name <email@example.com>
2018-09-19 13:06:20 +0800

    init

---
+++ homework2.9/homework2.9/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework2._9
+{
+    class Program
+    {
+       public  static void Main(string[] args)
+        {
+            Console.WriteLine("2到100之间所有的素数：");
+            int n = 0;
+            for(int i = 3; i < 100; i += 2)
+            {
+                for(int j = 2; j < i; j++)
+                {
+                    if (i % j == 0)
+                        goto outer;
+                }
+                Console.WriteLine(" " + 1);
+                n++;
+                if (n < 10)
+                    continue;
+                Console.WriteLine();
+                n = 0;
+            outer:;
+
+            }
+            Console.WriteLine();
+        }
+    }
+}
